334bcc4e00d101c9374e076acfc0472ae21fe4b5
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-05-11 21:28:04 +0500

    TransferParameters by value

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -4,9 +4,32 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+//#define POINTERS_BASICS
+
 void main()
 {
 	setlocale(LC_ALL, "");
 
+#ifdef POINTERS_BASICS
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;
+	cout << &a << endl;  // Взятие адреса переменной 'а' прямо при выводе
+	cout << pa << endl;  // Вывод адреса, хранящегося в указатели
+	cout << *pa << endl; // Разыменовываем указатель 'pa'  и получаем значение хранящееся по адресу
 
+	int* pb;
+	int b = 3;
+	pb = &b;
+#endif // POINTERS_BASICS
+
+	const int n = 5;
+	int arr[n] = { 3,5,8,13,21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
 }
---
+++ TransferParameters/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+void Exchange(int a, int b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2, b = 3;
+	cout << a << "\t" << b << endl;
+	cout << &a << "\t" << &b << endl;
+	Exchange(a, b);
+	cout << a << "\t" << b << endl;
+}
+void Exchange(int a, int b)
+{
+	cout << &a << "\t" << &b << endl;
+	int buffer = a;
+	a = b;
+	b = buffer;
+
+}
